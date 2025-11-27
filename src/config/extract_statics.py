eeea691a3d55075033be43b9badbe5140cbc9ddd
HaydenA0 <aregrag2005@gmail.com>
2025-11-27 18:49:37 +0100

    small fixes

--- src/config/extract_statics.py
+++ src/config/extract_statics.py
@@ -1,12 +1,12 @@
-#extract_statics.py
+# extract_statics.py
 
 import yaml
 
+
 # this function will return a dictionary-like element, you can access the information you want then
 def get_statics(subtree):
-    with open("statics.yaml") as f:
+    with open("src/config/statics.yaml") as f:
         cfg = yaml.safe_load(f)
 
-    section = cfg[subtree]   
+    section = cfg[subtree]
     return section
-
--- src/test.py
+++ src/test.py
@@ -1,17 +1,23 @@
 from cleaner.cleaner import main_cleaner
 import pandas as pd
+
+
 def main():
-    test_df = pd.DataFrame({
-        "name": [" Alice ", "Bob", "Alice ", None,"Charlie"],
-        "age": ["23", "30", None, "30", "23"],
-        "amount": ["100.5", None, "100.5", "100.5", None],
-        "date_str": ["2024-01-01", "notadate", None, "2022-05-05", "2024-01-01"],
-        "extra_nulls": [None, None, None, None,None] 
-    })
+    test_df = pd.DataFrame(
+        {
+            "name": [" Alice ", "Bob", "Alice ", None, "Charlie"],
+            "age": ["23", "30", None, "30", "23"],
+            "amount": ["100.5", None, "100.5", "100.5", None],
+            "date_str": ["2024-01-01", "notadate", None, "2022-05-05", "2024-01-01"],
+            "extra_nulls": [None, None, None, None, None],
+        }
+    )
 
     test_df.to_csv("sample_test.csv", index=False)
     df = main_cleaner("sample_test.csv")
     print("clean data:", df)
 
+
 if __name__ == "__main__":
     main()
+
